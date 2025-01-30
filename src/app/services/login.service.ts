2342df1c418650fcc588aee0afdb44fc98c39d00
DineshPatil025 <dineshbpatil74@gmail.com>
2025-01-30 12:07:15 +0530

    created registration froms

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IUser } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  baseURL: string = "https://yps-academy-42ea4-default-rtdb.firebaseio.com/";
+
+  constructor(private _http: HttpClient) { }
+
+
+
+  registerUser(user: IUser) {
+    console.log("new user details",user);    
+    return  this._http.post(this.baseURL + "users.json", user)
+  }
+
+
+  loginUser(user: IUser) {
+    console.log("existing user details",user);    
+    this._http.get(this.baseURL + "users.json").subscribe((res:any) => {
+      console.log(res);
+      for(let key in res){
+        if(res[key].email === user.email && res[key].password === user.password){
+          alert("user logged in");
+        }else{
+          alert("user not found");
+        }
+      }
+    })
+  }
 }
